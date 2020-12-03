dd5307290bf445955436b9c891351936f34a6448
Anton Saukh <grasshoppergn@gmail.com>
2020-12-03 14:21:31 +0100

    Initialize project using Create React App and add the sample implementation with Matter Element

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+// Replace with your API key
+const API_KEY = "mk_matter_sample";
+
+// An example portfolio query.
+const portfolioQuery: MatterElements.PortfolioQuery = {
+  ids: [
+    {
+      id: "MATTER_SAMPLE_PORTFOLIO_A",
+      type: "uid",
+      weight: 1500,
+    },
+    {
+      id: "MATTER_SAMPLE_PORTFOLIO_B",
+      type: "uid",
+      weight: 500,
+    },
+    {
+      id: "MATTER_SAMPLE_PORTFOLIO_C",
+      type: "uid",
+      weight: 123,
+    },
+  ],
+};
+
+function App() {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  // settings up Matter Elements API authorization keys
+  useEffect(() => {
+    fetch("https://api.thisismatter.com/elements/v1/auth/user_token", {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    })
+      .then((response) => response.json())
+      .then(({ token }) => {
+        MatterElements.setAuthToken(token);
+      })
+      .catch(() => {
+        alert("Cannot fetch authorization token for Matter Elements API");
+      });
+  }, []);
+
+  // a sample way to obtain the portfolio definition
+  const [portfolio, setPortfolio] = useState<
+    MatterElements.PortfolioQuery | undefined
+  >();
+
+  useEffect(() => {
+    // replace Promise construction with a fetch
+    Promise.resolve(portfolioQuery).then((result) => {
+      setPortfolio(result);
+    });
+  }, [setPortfolio]);
+
+  // create an element when the portfolio is ready. We don't need to wait for the API key to be ready.
+  useEffect(() => {
+    if (portfolio && containerRef.current) {
+      // create an instance of Single Impact controller
+      const singleImpactElement = MatterElements.singleImpact(
+        "co2", // "co2", "waste", "energy", "fossil"
+        portfolio,
+        {
+          // This CSS file will be loaded by the Matter Element and will alter the look
+          // and feel of that Element. We must use full paths here, because this file will be
+          // loaded from different origin. Also in production this file should allow
+          // https://elements.thisismatter.com/ origin to load it (with correct CORS headers).
+          cssUrls: [`${window.location.origin}/matter-style.css`],
+        }
+      );
+
+      // assign a container for rendering.
+      // At least the width of the container should be specified for the Element to be displayed.
+      // Some height is necessary to display the loading screen.
+      singleImpactElement.render(containerRef.current);
+      return singleImpactElement.destroy;
+    }
+    return undefined;
+  }, [portfolio]);
+
+  return (
+    <div className="App">
+      <div ref={containerRef} className="element-container" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/matter-elements.d.ts
@@ -0,0 +1,134 @@
+declare namespace MatterElements {
+  /**
+   * Creates an instance of a Single Impact Matter Element, that can be rendered into
+   * any container <div>.
+   *
+   * @param type Impact type.
+   * @param portfolio Portfolio definition that should be used to calculate the impact of.
+   * @param options Various options to customize the Element.
+   * @return An instance of the Element to render to a container.
+   */
+  function singleImpact(
+    type: "co2" | "fossil" | "waste" | "energy",
+    portfolio: PortfolioQuery,
+    options?: SingleImpactOptions
+  ): SingleImpact;
+
+  /**
+   * Sets authorization token. To get this token - request GET /auth/user_token
+   * method of the API with your API key.
+   *
+   * @param token - the API authorization token (a JWT string).
+   */
+  function setAuthToken(token: string): void;
+
+  // ----------- Aux interfaces -----------
+  interface SingleImpact {
+    /**
+     * Renders the Element to the specified container. This method is bound to the
+     * instance, so it's safe to pass it somewhere as a function.
+     */
+    render(container: HTMLElement): void;
+
+    /**
+     * Destroys the element, removing all its DOM. This method is bound to the
+     * instance, so it's safe to pass it somewhere as a function.
+     */
+    destroy(): void;
+  }
+
+  interface PortfolioQuery {
+    /**
+     * An array of portfolio identifiers with weights assigned to them.
+     */
+    ids: WeightedIdentifier[];
+  }
+
+  interface WeightedIdentifier {
+    /**
+     * Identifier type.
+     */
+    type: "isin" | "uid" | "uuid";
+    /**
+     * Identifier value.
+     */
+    id: string;
+    /**
+     * Weight of that portfolio in the result.
+     */
+    weight: number;
+  }
+
+  interface SingleImpactOptions {
+    /**
+     * An array of CSS file URLs to be used by the Element.
+     */
+    cssUrls?: string[];
+    /**
+     * An array of URLs to be preloaded as images to improve caching.
+     */
+    preloadImages?: string[];
+    /**
+     * An array of URLs to be preloaded as CSS files to improve caching.
+     */
+    preloadStyles?: string[];
+    /**
+     * An ordered array of page identifiers. The element will show just the pages specified.
+     */
+    pages?: string[];
+    /**
+     * Whether to reset the element to the first page, when the cursor leaves the Element.
+     */
+    noResetOnLeave?: boolean;
+    /**
+     * CSS class of the Next button icon. Can be set to "" to remove the icon character.
+     * Defaults to "next". Will be prefixed with "mtr-icon-" if set.
+     */
+    nextIcon?: string;
+    /**
+     * CSS class of the Done button icon. Can be set to "" to remove the icon character.
+     * Defaults to "checked". Will be prefixed with "mtr-icon-" if set.
+     */
+    doneIcon?: string;
+    /**
+     * CSS class of the Info button icon. Can be set to "" to remove the icon character.
+     * This button is not visible if there is no tooltip info for the page.
+     * Defaults to "question". Will be prefixed with "mtr-icon-" if set.
+     */
+    infoIcon?: string;
+    /**
+     * CSS class of the progress bar dot icon. Can be set to "" to remove the icon character.
+     * Defaults to "dot". Will be prefixed with "mtr-icon-" if set.
+     */
+    progressIcon?: string;
+    /**
+     * CSS class of the progress bar dot icon for the currently selected page.
+     * Can be set to "" to remove the icon character.
+     * Defaults to "long-dot". Will be prefixed with "mtr-icon-" if set.
+     */
+    progressCurrentIcon?: string;
+    /**
+     * Tooltip settings.
+     */
+    tooltip?: {
+      /**
+       * Either a string with a link to use on all pages, or an object with keys
+       * being page ids and values being links to use on corresponding pages.
+       */
+      readMoreLinks:
+        | string
+        | {
+            [key: string]: string;
+          };
+      /**
+       * Text for the tooltip button. Defaults to "Read more".
+       */
+      readMoreText?: string;
+      /**
+       * The value of the target attribute for the Read More button.
+       * Defaults to "_blank" which opens the link in the new tab.
+       */
+      readMoreOpenTarget?: string;
+    };
+  }
+}
