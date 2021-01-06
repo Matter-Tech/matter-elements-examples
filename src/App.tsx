681ead7cad7af585bb699875e7518acdcf2c5a7c
aksel <aksel@akseltorgard.com>
2021-01-06 15:20:26 +0000

    Reformat with prettier, and updated example data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,59 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 
+// Replace with your API key
+const API_KEY = "mk_matter_sample";
+
+// An example portfolio query.
+const portfolioQuery: MatterElements.PortfolioQuery = {
+  ids: [
+    {
+      id: "MATTER_SAMPLE_PORTFOLIO_A",
+      type: "uid",
+      weight: 1500,
+    },
+    {
+      id: "MATTER_SAMPLE_PORTFOLIO_B",
+      type: "uid",
+      weight: 500,
+    },
+    {
+      id: "MATTER_SAMPLE_PORTFOLIO_C",
+      type: "uid",
+      weight: 123,
+    },
+  ],
+};
+
 function App() {
   const containerRef = useRef<HTMLDivElement>(null);
 
   // settings up Matter Elements API authorization keys
   useEffect(() => {
-    fetch(
-        "https://api.thisismatter.com/elements/v1/auth/user_token",
-        {
-          headers: {
-            Authorization: `Bearer ${"mk_matter_sample" /* use your API key here */}`,
-          },
-        },
-    )
-        .then(response => response.json())
-        .then(({token}) => {
-          MatterElements.setAuthToken(token);
-        })
-        .catch(() => {
-          alert("Cannot fetch authorization token for Matter Elements API");
-        });
+    fetch("https://api.thisismatter.com/elements/v1/auth/user_token", {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    })
+      .then((response) => response.json())
+      .then(({ token }) => {
+        MatterElements.setAuthToken(token);
+      })
+      .catch(() => {
+        alert("Cannot fetch authorization token for Matter Elements API");
+      });
   }, []);
 
   // a sample way to obtain the portfolio definition
-  const [portfolio, setPortfolio] = useState<MatterElements.PortfolioQuery | undefined>(undefined);
+  const [portfolio, setPortfolio] = useState<
+    MatterElements.PortfolioQuery | undefined
+  >();
+
   useEffect(() => {
     // replace Promise construction with a fetch
-    Promise
-        .resolve({
-          ids: [
-            {
-              id: "AP_BAEREDYGTIG_2065",
-              type: "uid",
-              weight: 1,
-            },
-          ],
-        } as MatterElements.PortfolioQuery)
-        .then((result) => {
-          setPortfolio(result);
-        });
+    Promise.resolve(portfolioQuery).then((result) => {
+      setPortfolio(result);
+    });
   }, [setPortfolio]);
 
   // create an element when the portfolio is ready. We don't need to wait for the API key to be ready.
@@ -47,15 +61,15 @@ function App() {
     if (portfolio && containerRef.current) {
       // create an instance of Single Impact controller
       const singleImpactElement = MatterElements.singleImpact(
-          "co2", // "co2", "waste", "energy", "fossil"
-          portfolio,
-          {
-            // This CSS file will be loaded by the Matter Element and will alter the look
-            // and feel of that Element. We must use full paths here, because this file will be
-            // loaded from different origin. Also in production this file should allow
-            // https://elements.thisismatter.com/ origin to load it (with correct CORS headers).
-            cssUrls: [ `${window.location.origin}/matter-style.css` ]
-          },
+        "co2", // "co2", "waste", "energy", "fossil"
+        portfolio,
+        {
+          // This CSS file will be loaded by the Matter Element and will alter the look
+          // and feel of that Element. We must use full paths here, because this file will be
+          // loaded from different origin. Also in production this file should allow
+          // https://elements.thisismatter.com/ origin to load it (with correct CORS headers).
+          cssUrls: [`${window.location.origin}/matter-style.css`],
+        }
       );
 
       // assign a container for rendering.
@@ -68,9 +82,9 @@ function App() {
   }, [portfolio]);
 
   return (
-      <div className="App">
-        <div ref={containerRef} className="element-container" />
-      </div>
+    <div className="App">
+      <div ref={containerRef} className="element-container" />
+    </div>
   );
 }
 
